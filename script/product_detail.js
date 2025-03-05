7d201963c4ff3952d26b30ff7b9aa224ad809149
minhee <alsgml9154@naver.com>
2025-03-05 12:12:44 +0900

    Add files via upload

--- script/product_detail.js
+++ script/product_detail.js
@@ -4,8 +4,8 @@ const reviewSlide = new Swiper('.review_slide',{
     breakpoints:{
         1300: {slidesPerView:8,},
         700: {slidesPerView:6.4,},
-        360: {slidesPerView:4.2,},
-    },
+        300: {slidesPerView:4.2,},
+    }, 
 })
 console.log(reviewSlide)
 
